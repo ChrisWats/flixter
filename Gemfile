0fe27ce56b9eae2af5e013c3798da2e10c33d210
Chris Watson <watsonc135@gmail.com>
2019-10-19 06:38:35 +0000

    Added connections to stripe.

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ gem 'carrierwave', '~> 2.0'
 gem 'carrierwave-aws'
 gem "figaro"
 gem "fog-aws"
+gem 'stripe'
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,16 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :not_enrolled_yet, only: [:show]
   def show
   end
 
   private
+
+  def not_enrolled_yet
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must enroll before accessing the lessons.'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
